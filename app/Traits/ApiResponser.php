d280dc8f29ce0e31a0d6d971783ef315efdc562b
Juancho Garcia <juancho637@gmail.com>
2020-06-10 13:22:45 -0500

    generalización de respuestas para la API, integración de la paginación, ordenamiento, búsqueda y caché

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,6 +3,11 @@
 namespace App\Traits;
 
 use Illuminate\Http\Response;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 trait ApiResponser
 {
@@ -31,8 +36,126 @@ trait ApiResponser
     protected function successMessage($message, $code = 200)
     {
         return $this->jsonResponse([
-            'success' => $message,
+            'message' => $message,
             'code' => $code
         ], $code);
+    }
+
+    /**
+     * Generalización de las respuestas JSON exitosas para las colecciones de datos.
+     */
+    protected function showAll(Collection $collection, $code = 200)
+    {
+        if ($collection->isEmpty()) {
+            return $this->jsonResponse(['data' => $collection], $code);
+        }
+
+        if (isset($collection->first()->transformer)) {
+            $transformer = $collection->first()->transformer;
+
+            $collection = $this->filterData($collection, $transformer);
+            $collection = $this->sortData($collection, $transformer);
+            $collection = $this->paginateData($collection);
+            $collection = $this->transformData($collection, $transformer);
+            $collection = $this->cacheResponse($collection);
+        }
+
+        return $this->jsonResponse($collection, $code);
+    }
+
+    /**
+     * Generalización de las respuestas JSON exitosas para un objeto de la colección.
+     */
+    protected function showOne(Model $model, $code = 200)
+    {
+        $transformer = $model->first() ? $model->first()->transformer : $model->transformer;
+        $data = $this->transformData($model, $transformer);
+
+        return $this->jsonResponse($data, $code);
+    }
+
+    /**
+     * función para ordenar segun el campo sort_by de un request.
+     */
+    protected function sortData(Collection $collection, $transformer)
+    {
+        if (request()->has('sort_by')) {
+            $attribute = $transformer::originalAttribute(request()->sort_by);
+
+            $collection = $collection->sortBy->{$attribute};
+        }
+
+        return $collection;
+    }
+
+    /**
+     * función para filtrar segun el campo enviado en el request.
+     */
+    protected function filterData(Collection $collection, $transformer)
+    {
+        foreach (request()->query() as $query => $value) {
+            $attribute = $transformer::originalAttribute($query);
+
+            if (isset($attribute, $value)) {
+                $collection = $collection->where($attribute, $value);
+            }
+        }
+
+        return $collection;
+    }
+
+    /**
+     * función para la paginación.
+     */
+    protected function paginateData(Collection $collection)
+    {
+        $rules = [
+            'per_page' => 'integer|min:2|max:50'
+        ];
+        Validator::validate(request()->all(), $rules);
+
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        $perPage = 15;
+        if (request()->has('per_page')) {
+            $perPage = (int) request()->per_page;
+        }
+
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath()
+        ]);
+
+        $paginated->appends(request()->query());
+
+        return $paginated;
+    }
+
+    /**
+     * función para la cache.
+     */
+    protected function cacheResponse($data)
+    {
+        $url = request()->url();
+        $queryParams = request()->query();
+
+        ksort($queryParams);
+
+        $queryString = http_build_query($queryParams);
+        $fullUrl = "{$url}?{$queryString}";
+
+        return Cache::remember($fullUrl, 15/60, function () use ($data) {
+            return $data;
+        });
+    }
+
+    /**
+     * Generalización de las transformaciones para las respuestas JSON.
+     */
+    protected function transformData($data, $transformer)
+    {
+        $transformation = fractal($data, new $transformer);
+
+        return $transformation->toArray();
     }
 }
