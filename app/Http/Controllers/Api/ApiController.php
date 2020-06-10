5174b6018e5a863f6c0860552ef1d2cf39d640dd
Juancho Garcia <juancho637@gmail.com>
2020-06-10 13:23:17 -0500

    controlador general para la API

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Traits\ApiResponser;
+use App\Http\Controllers\Controller;
+
+class ApiController extends Controller
+{
+    use ApiResponser;
+
+    public function __construct()
+    {
+        //
+    }
+}
