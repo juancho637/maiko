6f70a0d271994cc02119adfd4a379b3489554d5a
Juancho Garcia <juancho637@gmail.com>
2020-06-18 12:36:17 -0500

    generalización de la configuración de data tables js

---
+++ public/js/datatables_config.js
@@ -0,0 +1,17 @@
+$.extend(true, $.fn.dataTable.defaults, {
+    "language": {
+        "info": "_TOTAL_ registros",
+        "search": "Buscar",
+        "lengthMenu": "Mostrar _MENU_ registros",
+        "paginate": {
+            "next": "Siguiente",
+            "previous": "Anterior"
+        },
+        "loadingRecords": "Cargando...",
+        "processing": "Procesando...",
+        "emptyTable": "No hay datos",
+        "zeroRecords": "No hay coinsidencias",
+        "infoEmpty": "",
+        "infoFiltered": ""
+    }
+});
