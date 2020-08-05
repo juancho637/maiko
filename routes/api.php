c7c6c97872db731cb2dbbad963afa62da4a0fe0d
Juancho Garcia <juancho637@gmail.com>
2020-08-05 18:26:58 -0500

    autoidentación

--- routes/api.php
+++ routes/api.php
@@ -23,120 +23,120 @@ Route::group(['middleware' => 'api'], function () {
         // Rutas para los estados de un recurso
         Route::apiResource('statuses', 'Api\Status\StatusController', [
             'only' => ['index', 'show'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para los países
         Route::apiResource('countries', 'Api\Country\CountryController', [
             'only' => ['index', 'show'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
         Route::apiResource('countries.states', 'Api\Country\CountryStateController', [
             'only' => ['index'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para los estados/departamentos
         Route::apiResource('states', 'Api\State\StateController', [
             'only' => ['index', 'show'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
         Route::apiResource('states.cities', 'Api\State\StateCityController', [
             'only' => ['index'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para las ciudades
         Route::apiResource('cities', 'Api\City\CityController', [
             'only' => ['index', 'show'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para los usuarios
         Route::apiResource('users.inspections', 'Api\User\UserInspectionController', [
             'only' => ['index'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para las empresas
         Route::apiResource('companies', 'Api\Company\CompanyController', [
             'only' => ['index', 'show'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
         Route::apiResource('companies.clients', 'Api\Company\CompanyClientController', [
             'only' => ['index', 'store'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para los clientes
         Route::apiResource('clients', 'Api\Client\ClientController', [
             'only' => ['show', 'update'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
         Route::apiResource('clients.tanks', 'Api\Client\ClientTankController', [
             'only' => ['index', 'store'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para los tanques
         Route::apiResource('tanks', 'Api\Tank\TankController', [
             'only' => ['show', 'update'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para las ordenes de trabajo
         Route::apiResource('work_orders', 'Api\WorkOrder\WorkOrderController', [
             'only' => ['index', 'show'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
         Route::apiResource('work_orders.inspections', 'Api\WorkOrder\WorkOrderInspectionController', [
             'only' => ['store'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
         Route::apiResource('users.work_orders', 'Api\User\UserWorkOrderController', [
             'only' => ['index'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para las inspecciones
         Route::apiResource('inspections', 'Api\Inspection\InspectionController', [
             'only' => ['show', 'update'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
         Route::post('inspections/{inspection}/complete', 'Api\Inspection\InspectionController@complete')
-                ->name('api.inspections.complete');;
+            ->name('api.inspections.complete');;
 
         // Rutas para las preguntas
         Route::apiResource('questions', 'Api\Question\QuestionController', [
             'only' => ['index', 'show'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para las respuestas
         Route::apiResource('answers', 'Api\Answer\AnswerController', [
             'only' => ['show', 'update'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
         Route::apiResource('inspections.answers', 'Api\Inspection\InspectionAnswerController', [
             'only' => ['index'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
         Route::apiResource('inspections.questions.answers', 'Api\Inspection\InspectionQuestionAnswerController', [
             'only' => ['store'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para las corrosiones
         Route::apiResource('corrosions', 'Api\Corrosion\CorrosionController', [
             'only' => ['show', 'update'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
         Route::apiResource('corrosions.files', 'Api\Corrosion\CorrosionFileController', [
-            'as'=>'api'
+            'as' => 'api'
         ]);
         Route::apiResource('inspections.corrosions', 'Api\Inspection\InspectionCorrosionController', [
             'only' => ['index', 'store'],
-            'as'=>'api'
+            'as' => 'api'
         ]);
 
         // Rutas para los accesorios
