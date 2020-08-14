4654aa774488110636accecd64d4868da0be28e2
Juancho Garcia <juancho637@gmail.com>
2020-08-14 13:55:28 -0500

    autoidentación

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -19,75 +19,75 @@ class PermissionsTableSeeder extends Seeder
         Permission::create(['name' => 'edit roles', 'module' => 'roles']);
         Permission::create(['name' => 'delete roles', 'module' => 'roles']);
 
-         // Users
-         Permission::create(['name' => 'view users', 'module' => 'users']);
-         Permission::create(['name' => 'view user', 'module' => 'users']);
-         Permission::create(['name' => 'create users', 'module' => 'users']);
-         Permission::create(['name' => 'edit users', 'module' => 'users']);
-         Permission::create(['name' => 'delete users', 'module' => 'users']);
+        // Users
+        Permission::create(['name' => 'view users', 'module' => 'users']);
+        Permission::create(['name' => 'view user', 'module' => 'users']);
+        Permission::create(['name' => 'create users', 'module' => 'users']);
+        Permission::create(['name' => 'edit users', 'module' => 'users']);
+        Permission::create(['name' => 'delete users', 'module' => 'users']);
 
-         // Countries
-         Permission::create(['name' => 'view countries', 'module' => 'countries']);
-         Permission::create(['name' => 'view country', 'module' => 'countries']);
-         Permission::create(['name' => 'create countries', 'module' => 'countries']);
-         Permission::create(['name' => 'edit countries', 'module' => 'countries']);
-         Permission::create(['name' => 'delete countries', 'module' => 'countries']);
+        // Countries
+        Permission::create(['name' => 'view countries', 'module' => 'countries']);
+        Permission::create(['name' => 'view country', 'module' => 'countries']);
+        Permission::create(['name' => 'create countries', 'module' => 'countries']);
+        Permission::create(['name' => 'edit countries', 'module' => 'countries']);
+        Permission::create(['name' => 'delete countries', 'module' => 'countries']);
 
-         // States
-         Permission::create(['name' => 'view states', 'module' => 'states']);
-         Permission::create(['name' => 'view state', 'module' => 'states']);
-         Permission::create(['name' => 'create states', 'module' => 'states']);
-         Permission::create(['name' => 'edit states', 'module' => 'states']);
-         Permission::create(['name' => 'delete states', 'module' => 'states']);
+        // States
+        Permission::create(['name' => 'view states', 'module' => 'states']);
+        Permission::create(['name' => 'view state', 'module' => 'states']);
+        Permission::create(['name' => 'create states', 'module' => 'states']);
+        Permission::create(['name' => 'edit states', 'module' => 'states']);
+        Permission::create(['name' => 'delete states', 'module' => 'states']);
 
-         // Cities
-         Permission::create(['name' => 'view cities', 'module' => 'cities']);
-         Permission::create(['name' => 'view city', 'module' => 'cities']);
-         Permission::create(['name' => 'create cities', 'module' => 'cities']);
-         Permission::create(['name' => 'edit cities', 'module' => 'cities']);
-         Permission::create(['name' => 'delete cities', 'module' => 'cities']);
+        // Cities
+        Permission::create(['name' => 'view cities', 'module' => 'cities']);
+        Permission::create(['name' => 'view city', 'module' => 'cities']);
+        Permission::create(['name' => 'create cities', 'module' => 'cities']);
+        Permission::create(['name' => 'edit cities', 'module' => 'cities']);
+        Permission::create(['name' => 'delete cities', 'module' => 'cities']);
 
-         // Companies
-         Permission::create(['name' => 'view companies', 'module' => 'companies']);
-         Permission::create(['name' => 'view company', 'module' => 'companies']);
-         Permission::create(['name' => 'create companies', 'module' => 'companies']);
-         Permission::create(['name' => 'edit companies', 'module' => 'companies']);
-         Permission::create(['name' => 'delete companies', 'module' => 'companies']);
+        // Companies
+        Permission::create(['name' => 'view companies', 'module' => 'companies']);
+        Permission::create(['name' => 'view company', 'module' => 'companies']);
+        Permission::create(['name' => 'create companies', 'module' => 'companies']);
+        Permission::create(['name' => 'edit companies', 'module' => 'companies']);
+        Permission::create(['name' => 'delete companies', 'module' => 'companies']);
 
-         // Clients
-         Permission::create(['name' => 'view clients', 'module' => 'clients']);
-         Permission::create(['name' => 'view client', 'module' => 'clients']);
-         Permission::create(['name' => 'create clients', 'module' => 'clients']);
-         Permission::create(['name' => 'edit clients', 'module' => 'clients']);
-         Permission::create(['name' => 'delete clients', 'module' => 'clients']);
+        // Clients
+        Permission::create(['name' => 'view clients', 'module' => 'clients']);
+        Permission::create(['name' => 'view client', 'module' => 'clients']);
+        Permission::create(['name' => 'create clients', 'module' => 'clients']);
+        Permission::create(['name' => 'edit clients', 'module' => 'clients']);
+        Permission::create(['name' => 'delete clients', 'module' => 'clients']);
 
-         // Tanks
-         Permission::create(['name' => 'view tanks', 'module' => 'tanks']);
-         Permission::create(['name' => 'view tank', 'module' => 'tanks']);
-         Permission::create(['name' => 'create tanks', 'module' => 'tanks']);
-         Permission::create(['name' => 'edit tanks', 'module' => 'tanks']);
-         Permission::create(['name' => 'delete tanks', 'module' => 'tanks']);
+        // Tanks
+        Permission::create(['name' => 'view tanks', 'module' => 'tanks']);
+        Permission::create(['name' => 'view tank', 'module' => 'tanks']);
+        Permission::create(['name' => 'create tanks', 'module' => 'tanks']);
+        Permission::create(['name' => 'edit tanks', 'module' => 'tanks']);
+        Permission::create(['name' => 'delete tanks', 'module' => 'tanks']);
 
-         // WorkOrders
-         Permission::create(['name' => 'view work orders', 'module' => 'work orders']);
-         Permission::create(['name' => 'view work order', 'module' => 'work orders']);
-         Permission::create(['name' => 'create work orders', 'module' => 'work orders']);
-         Permission::create(['name' => 'edit work orders', 'module' => 'work orders']);
-         Permission::create(['name' => 'delete work orders', 'module' => 'work orders']);
+        // WorkOrders
+        Permission::create(['name' => 'view work orders', 'module' => 'work orders']);
+        Permission::create(['name' => 'view work order', 'module' => 'work orders']);
+        Permission::create(['name' => 'create work orders', 'module' => 'work orders']);
+        Permission::create(['name' => 'edit work orders', 'module' => 'work orders']);
+        Permission::create(['name' => 'delete work orders', 'module' => 'work orders']);
 
-         // Inspections
-         Permission::create(['name' => 'view inspections', 'module' => 'inspections']);
-         Permission::create(['name' => 'view inspection', 'module' => 'inspections']);
+        // Inspections
+        Permission::create(['name' => 'view inspections', 'module' => 'inspections']);
+        Permission::create(['name' => 'view inspection', 'module' => 'inspections']);
         //  Permission::create(['name' => 'create inspections', 'module' => 'inspections']);
-         Permission::create(['name' => 'edit inspections', 'module' => 'inspections']);
-         Permission::create(['name' => 'delete inspections', 'module' => 'inspections']);
-         Permission::create(['name' => 'download inspection cert', 'module' => 'inspections']);
+        Permission::create(['name' => 'edit inspections', 'module' => 'inspections']);
+        Permission::create(['name' => 'delete inspections', 'module' => 'inspections']);
+        Permission::create(['name' => 'download inspection cert', 'module' => 'inspections']);
 
-         // Questions
-         Permission::create(['name' => 'view questions', 'module' => 'questions']);
-         Permission::create(['name' => 'view question', 'module' => 'questions']);
-         Permission::create(['name' => 'create questions', 'module' => 'questions']);
-         Permission::create(['name' => 'edit questions', 'module' => 'questions']);
-         Permission::create(['name' => 'delete questions', 'module' => 'questions']);
+        // Questions
+        Permission::create(['name' => 'view questions', 'module' => 'questions']);
+        Permission::create(['name' => 'view question', 'module' => 'questions']);
+        Permission::create(['name' => 'create questions', 'module' => 'questions']);
+        Permission::create(['name' => 'edit questions', 'module' => 'questions']);
+        Permission::create(['name' => 'delete questions', 'module' => 'questions']);
     }
 }
